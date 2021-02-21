4f2dd6e6cb1c6ed5f0fab4da2cb68afc64673add
Alex Recker <alex@reckerfamily.com>
2021-02-21 13:56:52 -0600

    Take out instagram

--- shmeeds/instagram.py
+++ shmeeds/instagram.py
@@ -1,7 +1,5 @@
-import InstagramAPI
-
 from shmeeds.log import logger
 
 
 def post(response, creds={}, dry=False):
-    logger.info('sharing story to instagram')
+    logger.info('authenticating to instagram')
