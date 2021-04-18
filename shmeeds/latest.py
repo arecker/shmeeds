3af8a7e46bd7cd22dab8a062a821708a2745a7ca
Alex Recker <alex@reckerfamily.com>
2021-04-17 23:03:42 -0500

    SHMEEDS harder

--- shmeeds/latest.py
+++ shmeeds/latest.py
@@ -13,10 +13,11 @@ def fetch(feed_url):
     logger.info('fetching RSS feed at %s', feed_url)
     feed = feedparser.parse(feed_url)
     latest = feed.entries[0]
+
     return collections.OrderedDict(
         bannerURL=latest['media_thumbnail'][0]['url'],
         permalink=latest['link'],
-        subtitle=latest['content'][-1]['value'],
+        subtitle=latest['summary'],
         title=latest['title']
     )
 
