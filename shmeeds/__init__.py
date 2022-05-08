35ff91c32f1d8c7bdeb67d50dd69dae29281167d
Alex Recker <alex@reckerfamily.com>
2022-05-07 22:29:49 -0500

    Make shmeeds great again

--- shmeeds/__init__.py
+++
@@ -1,7 +0,0 @@
-__version__ = '0.1.0'
-
-from shmeeds.cli import main
-
-
-if __name__ == '__main__':
-    main()
--- shmeeds/instagram.py
+++
@@ -1,5 +0,0 @@
-from shmeeds.log import logger
-
-
-def post(response, creds={}, dry=False):
-    logger.info('authenticating to instagram')
